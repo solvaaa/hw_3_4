393c59dea3f8613b741b6e2b6dd659373a6731f4
Alexandra Solovyova <alexandrasolovjova@mail.ru>
2023-05-17 01:08:06 +0300

    added title_ function, docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper_case(string):
+    """переводит строку в верхний регистр"""
     return string.upper()
+
+
+def title_(string):
+    """возвращает копию строки, где слова будут начинаться с заглавной буквы"""
+    return string.title()
